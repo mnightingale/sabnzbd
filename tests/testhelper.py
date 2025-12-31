87c0b185a5a57a167bcb18153eda657f72653370
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2025-12-31 21:35:30 +0000

    Revert connections

--- tests/testhelper.py
+++ tests/testhelper.py
@@ -317,11 +317,6 @@ class DownloadFlowBasics(SABnzbdBaseTest):
         port_inp.clear()
         port_inp.send_keys(SAB_NEWSSERVER_PORT)
 
-        # Change connections
-        port_inp = self.selenium_wrapper(self.driver.find_element, By.NAME, "connections")
-        port_inp.clear()
-        port_inp.send_keys(20)
-
         # Test server-check
         self.selenium_wrapper(self.driver.find_element, By.ID, "serverTest").click()
         self.wait_for_ajax()
