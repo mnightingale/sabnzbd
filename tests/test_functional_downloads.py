b790cef3065daa11824b310185d6c1f70af1fb57
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-01-01 08:59:25 +0000

    unique names

--- tests/test_functional_downloads.py
+++ tests/test_functional_downloads.py
@@ -24,23 +24,23 @@ from tests.testhelper import *
 @pytest.mark.repeat(10)
 class TestDownloadFlow(DownloadFlowBasics):
     def test_download_basic_rar5(self):
-        self.download_nzb("basic_rar5", ["My_Test_Download.bin"], True)
+        self.download_nzb("basic_rar5", ["My_Test_Download.bin"])
 
     def test_download_zip(self):
-        self.download_nzb("test_zip", ["My_Test_Download.bin"], True)
+        self.download_nzb("test_zip", ["My_Test_Download.bin"])
 
     def test_download_7zip(self):
-        self.download_nzb("test_7zip", ["My_Test_Download.bin"], True)
+        self.download_nzb("test_7zip", ["My_Test_Download.bin"])
 
     def test_download_passworded(self):
-        self.download_nzb("test_passworded{{secret}}", ["My_Test_Download.bin"], True)
+        self.download_nzb("test_passworded{{secret}}", ["My_Test_Download.bin"])
 
     def test_download_unicode_made_on_windows(self):
-        self.download_nzb("test_win_unicode", ["Frènch_German_Demö.bin"], True)
+        self.download_nzb("test_win_unicode", ["Frènch_German_Demö.bin"])
 
     def test_download_fully_obfuscated(self):
         # This is also covered by a unit test but added to test full flow
-        self.download_nzb("obfuscated_single_rar_set", ["My_Test_Download.bin"], True)
+        self.download_nzb("obfuscated_single_rar_set", ["My_Test_Download.bin"])
 
     def test_download_unicode_rar(self):
-        self.download_nzb("unicode_rar", ["我喜欢编程_My_Test_Download.bin"], True)
+        self.download_nzb("unicode_rar", ["我喜欢编程_My_Test_Download.bin"])
