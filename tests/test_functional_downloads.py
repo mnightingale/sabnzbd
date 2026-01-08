63496bf732abeb8d96486a53293d3fa5c7b2b8ce
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-01-08 16:25:59 +0000

    Without xfail

--- tests/test_functional_downloads.py
+++ tests/test_functional_downloads.py
@@ -34,7 +34,7 @@ class TestDownloadFlow(DownloadFlowBasics):
     def test_download_passworded(self):
         self.download_nzb("test_passworded{{secret}}", ["My_Test_Download.bin"])
 
-    @pytest.mark.xfail(reason="Probably #1633")
+    # @pytest.mark.xfail(reason="Probably #1633")
     def test_download_unicode_made_on_windows(self):
         self.download_nzb("test_win_unicode", ["frènch_german_demö.bin"])
 
