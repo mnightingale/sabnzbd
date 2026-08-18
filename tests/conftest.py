ab656093a563a2d16ad496364c6ed8e9257714e6
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2026-08-18 17:15:00 +0100

    Try using the built-in chrome driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -215,7 +215,6 @@ def run_sabnews_and_selenium(request):
 
     # Headless during CI testing
     if "CI" in os.environ:
-        driver_options.browser_version = "127"
         driver_options.add_argument("--headless")
         driver_options.add_argument("--no-sandbox")
 
