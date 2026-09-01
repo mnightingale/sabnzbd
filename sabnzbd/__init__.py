233dfe02366bf1b00dc85ef8703ad05db2f518df
mnightingale <9887246+mnightingale@users.noreply.github.com>
2026-09-01 20:17:14 +0100

    Apply username and password changes without a restart

--- sabnzbd/__init__.py
+++ sabnzbd/__init__.py
@@ -261,8 +261,6 @@ def initialize(pause_downloader=False, clean_up=False, repair=0):
     cfg.web_dir.callback(cfg.guard_restart)
     cfg.web_color.callback(cfg.guard_restart)
     cfg.url_base.callback(trigger_restart)
-    cfg.username.callback(cfg.guard_restart)
-    cfg.password.callback(cfg.guard_restart)
     cfg.log_dir.callback(cfg.guard_restart)
     cfg.https_port.callback(cfg.guard_restart)
     cfg.https_cert.callback(cfg.guard_restart)
